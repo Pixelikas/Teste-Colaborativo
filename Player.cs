4964fe9cd3399edf006216119b9ea91eb390e747
Pixelikas <lucasnaspolini@gmail.com>
2025-10-20 14:29:33 -0300

    add arquivos

---
+++ Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    int contagem = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        contagem++;
+        Debug.Log("Mouse por cima " + contagem + " vezes!");
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Nome do chão: " + col.gameObject.name);
+        Debug.Log("Nome do Player" + gameObject.name);
+        Destroy(gameObject);
+        Destroy(col.gameObject);
+    }
+
+}
